b8dc31e90c42f51f8bdd9634a8ff7079c3520091
Lady <lady@falahtech.co.id>
2026-05-13 16:26:24 +0700

    fix: dropfile comp, validation, link, remove file

--- components/ui/dropfile.tsx
+++ components/ui/dropfile.tsx
@@ -4,12 +4,12 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { cn, formatFileSize } from '@/lib/utils'
 import { ArrowUpToLine, FileText, X } from 'lucide-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export type FileOption = {
-  url: string
-  name: string
-  id: string
+  url?: string
+  name?: string
+  id?: string
   type?: string
   size?: number
 }
@@ -21,36 +21,68 @@ interface DropFileProps {
   onUploadFile?: (e: File[]) => void
   onRemoveFile?: () => void
   multiple?: boolean
+  files: FileOption[]
 }
 
 export default function DropFile({
   onUploadFile,
   onRemoveFile,
-  maxFilesSizeInMB = 0,
+  maxFilesSizeInMB = 5,
   multiple = false,
+  files: existingFile,
 }: DropFileProps) {
-  const [files, setFiles] = useState<File[]>([])
-  const isLimitExceed = files[0]?.size > maxFilesSizeInMB * 1_000_000
+  const MAX_FILE = maxFilesSizeInMB * 1_000_000
+
+  const [files, setFiles] = useState<FileOption[]>([])
+  const [isLimitExceed, setLimitExceed] = useState(false)
+  const [exist, setExist] = useState(existingFile)
+  const [isOnlyPdf, setOnlyPdf] = useState(false)
+  const displayedFiles = files.length > 0 ? files : exist
+  const failedFile = isLimitExceed || isOnlyPdf
+
   const handleFiles = (selectedFiles: FileList | null) => {
     if (!selectedFiles?.length) return
-    let fileArray = Array.from(selectedFiles)
-    if (!multiple) fileArray = [fileArray[0]]
-    if (isLimitExceed) setFiles([])
-    setFiles(fileArray)
 
-    if (!isLimitExceed && onUploadFile) {
-      onUploadFile(fileArray)
+    const fileArray = multiple ? Array.from(selectedFiles) : [selectedFiles[0]]
+    const notPdfFiles = fileArray.filter((file) => file.type !== 'application/pdf')
+    const limitExceedFiles = fileArray.filter((file) => file.size > MAX_FILE)
+
+    if (limitExceedFiles.length > 0) {
+      setFiles([])
+      setLimitExceed(true)
+      return
     }
+
+    if (notPdfFiles.length > 0) {
+      setFiles([])
+      setOnlyPdf(true)
+      return
+    }
+
+    onUploadFile?.(fileArray)
+    setFiles(fileArray)
+    setLimitExceed(false)
+    setOnlyPdf(false)
   }
-  console.log('FILES DALAM DROPFILE', files)
-  console.log('maxFilesSizeInMB', isLimitExceed)
+
+  const handleRemoveFiles = (idx: number) => {
+    if (files.length > 0) {
+      setFiles((prev) => prev.filter((_, i) => i !== idx))
+      return
+    }
+    setExist((prev) => prev.filter((_, i) => i !== idx))
+    onRemoveFile?.()
+  }
+
+  useEffect(() => {
+    setExist(existingFile)
+  }, [existingFile])
+
   return (
     <div>
       <div
         className={cn(
-          isLimitExceed
-            ? 'h-[182px] max-h-[182px] bg-red-200'
-            : 'h-[238px] max-h-[238px] bg-red-50',
+          failedFile ? 'h-[182px] max-h-[182px] bg-red-200' : 'h-[238px] max-h-[238px] bg-red-50',
           'relative mb-1.5 rounded-md'
         )}
       >
@@ -58,56 +90,67 @@ export default function DropFile({
           <div className='mb-4 flex justify-center text-center'>
             <div
               className={cn(
-                isLimitExceed ? 'border-error' : 'border-slate-400',
+                failedFile ? 'border-error' : 'border-slate-400',
                 'p- w-fit rounded-md border p-2'
               )}
             >
               <ArrowUpToLine
-                className={cn(isLimitExceed ? 'text-error' : 'text-slate-950', 'size-4')}
+                className={cn(failedFile ? 'text-error' : 'text-slate-950', 'size-4')}
               />
             </div>
           </div>
-          {isLimitExceed ? (
-            <div className='mb-4'>
-              <p className='text-error font-medium'>
-                Ukuran berkas lebih dari {maxFilesSizeInMB}MB
-              </p>
-            </div>
-          ) : (
-            <div className='mb-4'>
-              <p>
-                Klik untuk memilih berkas, atau seret <br /> berkas ke sini
-              </p>
-              <p className='text-slate-500'>
-                Berkas PDF (pdf), ukuran maksimal {maxFilesSizeInMB}MB per <br />
-                berkas
-              </p>
-            </div>
-          )}
+          <div className='mb-4'>
+            {isLimitExceed ? (
+              <div className='mb-4'>
+                <p className='text-error font-medium'>
+                  Ukuran berkas lebih dari {maxFilesSizeInMB}MB
+                </p>
+              </div>
+            ) : isOnlyPdf ? (
+              <div className='mb-4'>
+                <p className='text-error font-medium'>Berkas harus dalam format PDF</p>
+              </div>
+            ) : (
+              <>
+                <p>
+                  Klik untuk memilih berkas, atau seret <br /> berkas ke sini
+                </p>
+                <p className='text-slate-500'>
+                  Berkas PDF (pdf), ukuran maksimal {maxFilesSizeInMB}MB per <br />
+                  berkas
+                </p>
+              </>
+            )}
+          </div>
           <Button variant='secondary-outline'>
-            {isLimitExceed ? 'Unggah Berkas' : 'Ganti Berkas'}
+            {failedFile ? 'Unggah Berkas' : 'Ganti Berkas'}
           </Button>
         </div>
         <Input
           type='file'
           multiple={multiple}
-          accept='.pdf, application/pdf'
+          accept='application/pdf'
           className={cn(
-            isLimitExceed ? 'h-[182px] max-h-[182px]' : 'h-[238px] max-h-[238px]',
+            failedFile ? 'h-[182px] max-h-[182px]' : 'h-[238px] max-h-[238px]',
             'absolute top-0 right-0 bottom-0 left-0 w-full cursor-pointer border-dashed p-0 text-transparent file:hidden'
           )}
           onChange={(e) => handleFiles(e.target.files)}
         />
       </div>
-      {!isLimitExceed &&
-        files.length > 0 &&
-        files.map(({ name, size }, idx) => {
+      {!failedFile &&
+        displayedFiles.length > 0 &&
+        displayedFiles.map(({ name, size, url }, idx) => {
           return (
             <div
               key={idx}
               className='flex h-[88px] w-full cursor-pointer items-center justify-between rounded-md border border-dashed border-slate-400 bg-red-50 p-6'
             >
-              <div className='flex items-center gap-2'>
+              <a
+                href={url}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='flex items-center gap-2'
+              >
                 <div className='w-fit rounded-md border border-slate-400 p-2 text-center'>
                   <FileText className='size-4' />
                 </div>
@@ -115,11 +158,11 @@ export default function DropFile({
                   <p className='text-sm font-medium'>{name}</p>
                   <p className='text-sm text-slate-500'>{formatFileSize(size || 0)}</p>
                 </div>
-              </div>
+              </a>
               <Button
                 variant='ghost'
                 className='text-error hover:text-error p-0 hover:bg-transparent'
-                onClick={onRemoveFile}
+                onClick={() => handleRemoveFiles(idx)}
               >
                 <X className='size-5' />
               </Button>
