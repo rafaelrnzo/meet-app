4d8cbec138287387f69fecc8de9403f92af50e05
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2026-05-22 15:48:10 +0700

    refactor: update styling for textarea, button, input, and input group components

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
+        "flex field-sizing-content min-h-16 w-full rounded-md border transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/50 dark:bg-input/30  border-neutral-400 bg-transparent px-2 py-1 text-md shadow tracking-normal disabled:placeholder:text-slate-700 hover:bg-neutral-50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 font-normal text-neutral-950 disabled:bg-slate-300 text-md aria-invalid:ring-red-200 dark:aria-invalid:ring-destructive/40 aria-invalid:border-red-200 aria-invalid:bg-red-200 aria-invalid:text-error",
         className
       )}
       {...props}
