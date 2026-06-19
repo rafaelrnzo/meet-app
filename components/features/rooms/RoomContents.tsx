8af573f44abd10ba568f49998cc059a801de0426
Lady <lady@falahtech.co.id>
2026-06-19 16:16:43 +0700

    fix: implement permission room detail

--- components/features/rooms/RoomContents.tsx
+++ components/features/rooms/RoomContents.tsx
@@ -56,7 +56,8 @@ function OverviewContent({
   handleUploadFile,
   handleRemoveFile,
 }: RoomContentsProps['overview']) {
-  const { isAdmin } = useAuth()
+  const { hasPermission } = useAuth()
+  const canManage = hasPermission('room:manage')
   return (
     <div className='animate-in fade-in slide-in-from-bottom-4 mt-4 duration-300'>
       <div
@@ -95,33 +96,32 @@ function OverviewContent({
           <p className='text-xs'>{room?.max_participants ?? 0} peserta</p>
         </div>
       </div>
-      {isAdmin && (
-        <div>
-          <div className='my-2'>
-            <p className='pb-2'>Deskripsi ruangan</p>
-            <div className='min-h-16 rounded-md border border-slate-400 px-3 py-1 wrap-break-word whitespace-pre-wrap shadow-sm'>
-              {room?.description || '-'}
-            </div>
-          </div>
-          <div className='my-2'>
-            <p className='pb-2'>Unggah berkas presentasi</p>
-            <DropFile
-              files={
-                files.map((item) => ({
-                  name: item.file_name,
-                  url: item.file_url,
-                  size: item.size,
-                })) || []
-              }
-              maxFilesSizeInMB={MAX_FILE}
-              onUploadFile={(files) => {
-                handleUploadFile(files)
-              }}
-              onRemoveFile={handleRemoveFile}
-            />
+      <div>
+        <div className='my-2'>
+          <p className='pb-2'>Deskripsi ruangan</p>
+          <div className='min-h-16 rounded-md border border-slate-400 px-3 py-1 wrap-break-word whitespace-pre-wrap shadow-sm'>
+            {room?.description || '-'}
           </div>
         </div>
-      )}
+        <div className='my-2'>
+          <p className='pb-2'>Unggah berkas presentasi</p>
+          <DropFile
+            canUpload={canManage}
+            files={
+              files.map((item) => ({
+                name: item.file_name,
+                url: item.file_url,
+                size: item.size,
+              })) || []
+            }
+            maxFilesSizeInMB={MAX_FILE}
+            onUploadFile={(files) => {
+              handleUploadFile(files)
+            }}
+            onRemoveFile={handleRemoveFile}
+          />
+        </div>
+      </div>
     </div>
   )
 }
@@ -134,7 +134,8 @@ function ParticipantsContent({
   setIsOpenBlock,
   setUserIdentity,
 }: RoomContentsProps['participants']) {
-  const { isAdmin } = useAuth()
+  const { isAdmin, hasPermission } = useAuth()
+  const canManage = hasPermission('room:manage')
   return (
     <div>
       <div className='animate-in fade-in slide-in-from-bottom-4 mt-4 duration-300'>
@@ -169,96 +170,98 @@ function ParticipantsContent({
           )}
         </div>
       </div>
-      <div className='mb-2'>
-        <div className='my-4'>
-          <p className='mb-2'>Peserta ruangan</p>
-          <div className='flex items-center justify-between gap-2'>
-            <InputGroup className='has-[[data-slot][aria-invalid=true]]:[&>input]:text-error flex max-h-9 w-full items-center gap-2 rounded-md border border-neutral-400 px-3 py-[7.5px] shadow-sm has-[[data-slot][aria-invalid=true]]:border-red-200 has-[[data-slot][aria-invalid=true]]:bg-red-200'>
-              <InputGroupInput
-                aria-invalid={!!searchParticipants.value && !allParticipants.users.length}
-                placeholder='Cari peserta ...'
-                className='h-auto'
-                {...searchParticipants}
-              />
-              <InputGroupAddon>
-                <Icon type='search' className='size-4 text-neutral-400' />
-              </InputGroupAddon>
-            </InputGroup>
-            <Select
-              value={filterParticipants.value}
-              onValueChange={filterParticipants.onValueChange}
-            >
-              <SelectTrigger className='w-fit cursor-pointer border border-neutral-400 [&>svg]:last:hidden'>
-                <Icon type='filter' className='text-neutral-950!' />
-              </SelectTrigger>
-              <SelectContent position='popper' className='max-w-[187px] wrap-anywhere'>
-                <SelectGroup>
-                  <SelectItem value='all'>Semua</SelectItem>
-                  <SelectItem value='waiting'>Menunggu persetujuan</SelectItem>
-                  <SelectItem value='banned'>Diblokir</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+      {canManage && (
+        <div className='mb-2'>
+          <div className='my-4'>
+            <p className='mb-2'>Peserta ruangan</p>
+            <div className='flex items-center justify-between gap-2'>
+              <InputGroup className='has-[[data-slot][aria-invalid=true]]:[&>input]:text-error flex max-h-11 w-full items-center gap-2 rounded-md border border-neutral-400 px-3 py-[7.5px] shadow-sm has-[[data-slot][aria-invalid=true]]:border-red-200 has-[[data-slot][aria-invalid=true]]:bg-red-200'>
+                <InputGroupInput
+                  aria-invalid={!!searchParticipants.value && !allParticipants.users.length}
+                  placeholder='Cari peserta ...'
+                  className='h-auto'
+                  {...searchParticipants}
+                />
+                <InputGroupAddon>
+                  <Icon type='search' className='size-4 text-neutral-400' />
+                </InputGroupAddon>
+              </InputGroup>
+              <Select
+                value={filterParticipants.value}
+                onValueChange={filterParticipants.onValueChange}
+              >
+                <SelectTrigger className='w-fit cursor-pointer border border-neutral-400 [&>svg]:last:hidden'>
+                  <Icon type='filter' className='text-neutral-950!' />
+                </SelectTrigger>
+                <SelectContent position='popper' className='max-w-[187px] wrap-anywhere'>
+                  <SelectGroup>
+                    <SelectItem value='all'>Semua</SelectItem>
+                    <SelectItem value='waiting'>Menunggu persetujuan</SelectItem>
+                    <SelectItem value='banned'>Diblokir</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-        </div>
-        {allParticipants.users.length <= 0 ? (
-          <NoData
-            title='Tidak Ada Peserta yang Menunggu Persetujuan maupun yang Diblokir'
-            className='max-h-[calc(100vh-550px)] min-h-[calc(100vh-650px)]'
-          />
-        ) : (
-          <div className='overflow-auto'>
-            {allParticipants.users.map((user, idx) => {
-              const isWaiting = user.room_presence === 'waiting'
-              return (
-                <div
-                  key={idx}
-                  className={cn(
-                    isWaiting ? 'border-neutral-950' : 'border-error',
-                    'my-1 flex h-14 items-center justify-between gap-2 rounded-md border px-5 py-3 transition-colors'
-                  )}
-                >
-                  <div className='flex items-center gap-2'>
-                    {isWaiting ? (
-                      <Icon type='hourglass' className='size-[19.5px] text-neutral-950' />
-                    ) : (
-                      <Icon type='block' className='text-error size-[19.5px]' />
+          {allParticipants.users.length <= 0 ? (
+            <NoData
+              title='Tidak Ada Peserta yang Menunggu Persetujuan maupun yang Diblokir'
+              className='max-h-[calc(100vh-550px)] min-h-[calc(100vh-650px)]'
+            />
+          ) : (
+            <div className='overflow-auto'>
+              {allParticipants.users.map((user, idx) => {
+                const isWaiting = user.room_presence === 'waiting'
+                return (
+                  <div
+                    key={idx}
+                    className={cn(
+                      isWaiting ? 'border-neutral-950' : 'border-error',
+                      'my-1 flex h-14 items-center justify-between gap-2 rounded-md border px-5 py-3 transition-colors'
                     )}
-                    <span
-                      className={cn(
-                        isWaiting ? 'text-neutral-950' : 'text-error',
-                        'text-sm font-medium'
+                  >
+                    <div className='flex items-center gap-2'>
+                      {isWaiting ? (
+                        <Icon type='hourglass' className='size-[19.5px] text-neutral-950' />
+                      ) : (
+                        <Icon type='block' className='text-error size-[19.5px]' />
                       )}
-                    >
-                      {user.username}
-                    </span>
-                  </div>
-                  {isWaiting ? (
-                    <span className='font-semibold text-neutral-400 opacity-50'>
-                      Menunggu persetujuan
-                    </span>
-                  ) : (
-                    isAdmin && (
-                      <Button
-                        variant='destructive'
-                        className='text-error flex items-center gap-2'
-                        onClick={() => {
-                          onClose()
-                          setIsOpenBlock(true)
-                          setUserIdentity(user.username)
-                        }}
+                      <span
+                        className={cn(
+                          isWaiting ? 'text-neutral-950' : 'text-error',
+                          'text-sm font-medium'
+                        )}
                       >
-                        <Icon type='lock-open' className='text-error size-4' />
-                        Buka Blokir
-                      </Button>
-                    )
-                  )}
-                </div>
-              )
-            })}
-          </div>
-        )}
-      </div>
+                        {user.username}
+                      </span>
+                    </div>
+                    {isWaiting ? (
+                      <span className='font-semibold text-neutral-400 opacity-50'>
+                        Menunggu persetujuan
+                      </span>
+                    ) : (
+                      isAdmin && (
+                        <Button
+                          variant='destructive'
+                          className='text-error flex items-center gap-2'
+                          onClick={() => {
+                            onClose()
+                            setIsOpenBlock(true)
+                            setUserIdentity(user.username)
+                          }}
+                        >
+                          <Icon type='lock-open' className='text-error size-4' />
+                          Buka Blokir
+                        </Button>
+                      )
+                    )}
+                  </div>
+                )
+              })}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
