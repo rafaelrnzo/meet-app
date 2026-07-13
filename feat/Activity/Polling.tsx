7051b9fb0a628ec521b21be23e1c10e97ffe7e42
Nabilah Nabilah <nabilah9626@gmail.com>
2026-07-13 02:33:27 +0000

    [SISFO BAHASA KONFERENSI KELAS][FIX] - Modul Alat Rapat Sub Modul Pendapat
    **Ticket:**
    
    * https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00923
    
    **Changelogs:**
    
    * fix polling hilang setelah rejoin room - #00923
    * fix moderator termasuk host, tidak bisa isi polling. admin dan moderator bisa saling menutup polling

--- feat/Activity/Polling.tsx
+++ feat/Activity/Polling.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import type { FC } from 'react'
-import { usePollingSession } from '@/hooks'
+import { useLocalParticipant } from '@livekit/components-react'
+import { useDataChannel, usePollingSession } from '@/hooks'
+import { LiveKitAction } from '@/feat/enum'
 import { PollingCard } from '@/components/PollingCard'
 
 export const Polling: FC<{ onReady?: () => void }> = ({ onReady }) => {
   const {
+    identity,
     question,
     options,
     openedAt,
@@ -16,6 +19,19 @@ export const Polling: FC<{ onReady?: () => void }> = ({ onReady }) => {
     findVote,
     endPolling,
   } = usePollingSession(onReady)
+  const { localParticipant } = useLocalParticipant()
+
+  const { send: dataChannelClosePoll } = useDataChannel<string>(LiveKitAction.ClosePolling, () =>
+    endPolling()
+  )
+
+  const handleClosePolling = () => {
+    if (!identity) return
+    if (localParticipant.identity !== identity) {
+      return dataChannelClosePoll(identity, { reliable: false, destinationIdentities: [identity] })
+    }
+    endPolling()
+  }
 
   return (
     <div className='flex h-full w-full items-center justify-center overflow-hidden bg-white'>
@@ -29,7 +45,7 @@ export const Polling: FC<{ onReady?: () => void }> = ({ onReady }) => {
           isResult={isHost}
           checked={findVote()}
           onCheckedChange={selectVote}
-          onClosePolling={endPolling}
+          onClosePolling={handleClosePolling}
         />
       </div>
     </div>
