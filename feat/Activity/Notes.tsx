5ae9a59cb88e3000ab12543f6d1ebefb190a5518
Lady Vialistha <vialisthalady@gmail.com>
2026-07-16 08:52:47 +0000

    [SISFO BAHASA KONFERENSI KELAS][FIX] - Modul Alat Rapat Sub Modul Berbagi Catatan
    **Ticket**:
    - [Modul Alat Rapat](https://sd-erp.falahtech.com/app/task/Modul%20Alat%20Rapat%20-%20FE%20Sisfo%20Bahasa%20Konferensi%20Kelas)
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00975
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00974
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00971
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00959
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00919
    - https://sd-erp.falahtech.com/app/sd-issue/ISS-2026-00931
    
    **Changelog**:
    - Share notes - Warna indikator aktif pada format text blm sesuai #00975
    - Share notes - blm ada place holder #00974
    - Share notes - Peserta  masih bisa edit share notes #00971
    - Share notes - indikator username muncul di hasiil download pdf #00959
    - Share Notes - Tidak bisa langsung mengganti format teks #00919
    - Share Screen - klik cancel presentasi menampilkan toast error` #00931
    - Fix room identity by user id untuk hit api open banned

--- feat/Activity/Notes.tsx
+++ feat/Activity/Notes.tsx
@@ -7,11 +7,11 @@ import { NotesToolbarEditor } from '@/components/NotesToolbar'
 import '@/app/prose.css'
 
 export const Notes: FC<{ onReady?: () => void }> = ({ onReady }) => {
-  const { viewRef, editor, setEditor } = useNotes({ onReady })
+  const { viewRef, editor, isEditable, setEditor } = useNotes({ onReady })
 
   return (
     <div className='absolute inset-0 flex flex-col bg-white text-black'>
-      <NotesToolbarEditor getView={() => viewRef.current} editorEl={editor} />
+      {isEditable && <NotesToolbarEditor getView={() => viewRef.current} editorEl={editor} />}
       <div ref={setEditor} className='h-full w-full flex-1 overflow-auto p-5' />
     </div>
   )
